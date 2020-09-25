4ceb49eb7b528c30cfd14efb437b9b7b60cba43d
zql95 <1003215343@qq.com>
2020-09-25 13:31:49 +0800

    第一次提交day09

---
+++ day09/src/cn/itcast/demo04/PersonTest.java
@@ -0,0 +1,20 @@
+package cn.itcast.demo04;
+/*
+ * 定义好的Person类进行测试
+ * 创建对象，对象调用属性和方法
+ */
+public class PersonTest {
+	public static void main(String[] args) {
+		//创建Person类的对象 new
+		Person p=new Person();
+		//对成员变量赋值
+		p.name="张三";
+		//对成员变量age赋值，只能调用set方法赋值
+		p.setAge(50);
+		//调用类中方法
+		p.speak();
+		
+		//输出成员变量age值，必须调用get方法
+		System.out.println(p.getAge());
+	}
+}
